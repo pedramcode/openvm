d5772b11f2b250957f9ec58466fb42dc2445a82f
pedramcode <dev.dehghanpour@gmail.com>
2025-03-20 19:37:13 +0330

    fixed clean and added assembler lib

---
+++ lib/assembler/include/assembler/echo.h
@@ -0,0 +1,6 @@
+#ifndef ASSEMBLER_ECHO_H
+#define ASSEMBLER_ECHO_H
+
+void echo(char *message);
+
+#endif
---
+++ lib/assembler/src/echo.c
@@ -0,0 +1,6 @@
+#include <assembler/echo.h>
+#include <stdio.h>
+
+void echo(char *message){
+    printf("ECHO: %s\n", message);
+}
